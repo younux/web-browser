b99adba911b63738fe82e7e588c2caaed1dd5b11
younux <benhoumich.younes@gmail.com>
2018-02-04 22:06:46 +0100

    Init the project structure

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # web-browser
+
+This is a simple web browser developed using C++ and Qt 5.10.0. framework.
+
+The project was built using qmake on Qt Creator.
---
+++ web-browser/webpage.h
@@ -0,0 +1,17 @@
+#ifndef WEBPAGE_H
+#define WEBPAGE_H
+
+#include <QWidget>
+
+class WebPage : public QWidget
+{
+    Q_OBJECT
+public:
+    explicit WebPage(QWidget *parent = nullptr);
+
+signals:
+
+public slots:
+};
+
+#endif // WEBPAGE_H
